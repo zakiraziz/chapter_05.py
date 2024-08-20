a9f938a8edb8f7c9d7a5b5216f1a35ee24358395
zakir <azizizakir449@gmail.com>
2024-08-20 20:34:28 +0500

    my commit

---
+++ 01_dict.py
@@ -0,0 +1,8 @@
+marks = {
+    "zakir": 100,
+    "shubham": 46,
+    "harry": 30,
+}
+
+# print(marks, type(marks))
+print(marks["zakir"])
---
+++ 02_dict_mathods.py
@@ -0,0 +1,6 @@
+marks = {
+    "Zakir": 100,
+    "Shubham": 57,
+    "Rohan": 43,
+    0: "Zakir"
+}
