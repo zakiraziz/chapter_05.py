b8d2498cad161d04fe0dbfaff8ffc55b06149248
zakir <azizizakir449@gmail.com>
2024-08-22 21:49:17 +0500

    my commit

--- 02_dict_mathods.py
+++ 02_dict_mathods.py
@@ -4,3 +4,12 @@ marks = {
     "Rohan": 43,
     0: "Zakir"
 }
+
+#  print(marks.items())
+# print(marks.keys())
+#  print(marks.values())
+# marks.update({"Zakir": 99, "Harry": 100,})
+# print(marks)
+
+print(marks.get("Zakir2")) # prints None
+print(marks["Zakir2"]) # returns an eroor
