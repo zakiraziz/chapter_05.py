ecf8b6738b45e523265c34de0b97f99eeceec854
zakir <azizizakir449@gmail.com>
2024-08-23 21:00:21 +0500

    my commit

--- 03_sets.py
+++ 03_sets.py
@@ -1,3 +1,5 @@
-s = {1, 5, 32}
-
 e = set() # Dont use s = {} as it will creat an empty distioanry
+
+s = {1, 5, 32, 54,5, 5, 5}
+
+print(s)
---
+++ 04_set_methods.py
@@ -0,0 +1,6 @@
+s = {1, 5, 32, 54,5, 5, 5, "Zakir"}
+
+print(s,type(s))
+
+s.add("Harry")
+print(s,type(s))
